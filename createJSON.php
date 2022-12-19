0b5ea3d668a9cf3e59a75db0628955cc4bef46d5
uc-sps <surya.pratap@ucertify.com>
2022-12-19 12:14:58 +0530

    feedback done

--- createJSON.php
+++ createJSON.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * File-name : PHP_Project/createJSON.php
+ * Created at : 12/15/2022
+ * @detail : responsible for creating a empty answer file
+ * Last Updated by : Surya Pratap Singh <surya.pratap@ucertify.com>
+ * last Updated Date : 12/19/2022
+ */
 $n = $_POST['NumberOfQuestion'];
 $data = file_get_contents('answer.json');
 $json = json_decode($data);
--- data.php
+++ data.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * File-name : PHP_Project/data.php
+ * Created at : 12/15/2022
+ * @detail : responsible for saving data
+ * Last Updated by : Surya Pratap Singh <surya.pratap@ucertify.com>
+ * last Updated Date : 12/19/2022
+ */
 $que = $_POST['questionNo'];
 $ans = $_POST['choice'];
 $old = file_get_contents('answer.json');
